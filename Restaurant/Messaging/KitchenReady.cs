d7a9e15810badd07892aadf579acc395e214107b
Xyliolol <93090627+Xyliolol@users.noreply.github.com>
2022-10-23 16:48:26 +0300

    Работа с MassTransit

---
+++ Restaurant/Messaging/KitchenReady.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Messages
+{
+    public interface IKitchenReady
+    {
+        public Guid OrderId { get; }
+
+        public bool Ready { get; }
+    }
+
+    public class KitchenReady : IKitchenReady
+    {
+        public KitchenReady(Guid orderId, bool ready)
+        {
+            OrderId = orderId;
+            Ready = ready;
+        }
+
+        public Guid OrderId { get; }
+        public bool Ready { get; }
+    }
+}
--- Restaurant/Restaurant.Notification/Program.cs
+++ Restaurant/Restaurant.Notification/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MassTransit;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Restaurant.Notification.Consumers;
 
 namespace Restaurant.Notification
 {
@@ -15,11 +14,36 @@ namespace Restaurant.Notification
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
-                    services.AddHostedService<Worker>();
+                    services.AddMassTransit(x =>
+                    {
+                        x.AddConsumer<NotifierTableBookedConsumer>();
+                        x.AddConsumer<KitchenReadyConsumer>();
+
+                        x.UsingRabbitMq((context, cfg) =>
+                        {
+                            cfg.UseMessageRetry(r =>
+                            {
+                                r.Exponential(5,
+                                    TimeSpan.FromSeconds(1),
+                                    TimeSpan.FromSeconds(100),
+                                    TimeSpan.FromSeconds(5));
+                                r.Ignore<StackOverflowException>();
+                                r.Ignore<ArgumentNullException>(x => x.Message.Contains("Consumer"));
+                            });
+
+
+                            cfg.ConfigureEndpoints(context);
+                        });
+
+
+
+                    });
+                    services.AddSingleton<Notifier>();
+                    services.AddMassTransitHostedService(true);
                 });
     }
 }
