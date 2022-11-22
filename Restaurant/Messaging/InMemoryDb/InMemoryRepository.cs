5f19420af06e8e559c521e0f7232b5965c24231f
Xyliolol <93090627+Xyliolol@users.noreply.github.com>
2022-11-22 15:29:41 +0300

    Урок 6. Дублирование сообщений. Идемпотентность

---
+++ Restaurant/Messaging/InMemoryDb/InMemoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Messages.InMemoryDb
+{
+    public class InMemoryRepository<T> : IInMemoryRepository<T> where T : class
+    {
+        private readonly ConcurrentBag<T> _repo = new();
+
+        public void AddOrUpdate(T entity)
+        {
+            _repo.Add(entity);
+        }
+
+        public IEnumerable<T> Get()
+        {
+            return _repo;
+        }
+    }
+}
--- Restaurant/Restaurant.Booking/Consumers/BookingRequestConsumer.cs
+++ Restaurant/Restaurant.Booking/Consumers/BookingRequestConsumer.cs
@@ -1,5 +1,6 @@
 ﻿using MassTransit;
 using Restaurant.Messages;
+using Restaurant.Messages.InMemoryDb;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,17 +12,36 @@ namespace Restaurant.Booking.Consumers
     public class RestaurantBookingRequestConsumer : IConsumer<IBookingRequest>
     {
         private readonly Restaurant _restaurant;
+        private readonly IInMemoryRepository<BookingRequestModel> _repository;
 
-        public RestaurantBookingRequestConsumer(Restaurant restaurant)
+
+        public RestaurantBookingRequestConsumer(Restaurant restaurant,
+            IInMemoryRepository<BookingRequestModel> repository)
         {
             _restaurant = restaurant;
+            _repository = repository;
         }
 
         public async Task Consume(ConsumeContext<IBookingRequest> context)
         {
-            Console.WriteLine($"[OrderId: {context.Message.OrderId}]");
-            var result = await _restaurant.BookFreeTableAsync(1);
+            var model = _repository.Get().FirstOrDefault(i => i.OrderId == context.Message.OrderId);
 
+            if (model is not null && model.CheckMessageId(context.MessageId.ToString()))
+            {
+                Console.WriteLine(context.MessageId.ToString());
+                Console.WriteLine("Second time");
+                return;
+            }
+
+            var requestModel = new BookingRequestModel(context.Message.OrderId, context.Message.ClientId,
+                context.Message.PreOrder, context.Message.CreationDate, context.MessageId.ToString());
+
+            Console.WriteLine(context.MessageId.ToString());
+            Console.WriteLine("First time");
+            var resultModel = model?.Update(requestModel, context.MessageId.ToString()) ?? requestModel;
+
+            _repository.AddOrUpdate(resultModel);
+            var result = await _restaurant.BookFreeTableAsync(1);
             await context.Publish<ITableBooked>(new TableBooked(context.Message.OrderId, result ?? false));
         }
     }
